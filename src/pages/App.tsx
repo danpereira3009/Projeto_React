12cf749e3198033574e39c4200a8d962a49f4df9
daniel <danpereira3009@gmail.com>
2022-04-27 16:24:58 -0300

    first commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,19 @@
+//Sempre importar o componente e adicionar na function App()
+
+import React from 'react';
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista'
+import style from './App.module.scss';
+
+
+
+function App() {
+  return (
+    <div className={style.AppStyle}>
+      <Formulario />
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
