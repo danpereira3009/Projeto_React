ff0fbf6a1e1a6daced69ad14a982a836efd189c2
daniel <danpereira3009@gmail.com>
2022-04-27 20:11:16 -0300

    adição de setTarefas

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,18 +1,21 @@
 //Sempre importar o componente e adicionar na function App()
 
-import React from 'react';
+
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista'
 import style from './App.module.scss';
 import Cronometro from '../components/Cronometro'
+import React, { useState } from 'react'
+import { ITarefa } from '../types/tarefas';
 
 
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas}/>
+      <Lista tarefas={tarefas}/>
       <Cronometro />
     </div>
   );
